ffe9f189e897de750a15c6e8493f42d5e4c4e1e4
Claudia Delgado <claudiadelgado@Claudias-Air.localdomain>
2024-05-29 09:27:53 -0600

    wb news

--- src/components/HackerNews/HackerNews.jsx
+++ src/components/HackerNews/HackerNews.jsx
@@ -1,11 +1,36 @@
-import { useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
+import { requestArticles } from '../../reducers/hackerNewsReducer.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+
+
+
+
 
 export default function HackerNews() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [articles, setArticles] = useState([]);// we use useSelector instead of this local states
+  // const [loading, setLoading] = useState(true);
 
+    const articles = useSelector((state) => {
+      console.log(state);
+      return state.hackerNews.articles
+    });
+
+    const loading = useSelector((state) => {
+      return state.hackerNews.loading
+    });
+
+    const dispatch = useDispatch();
+    
+
+    useEffect(() => {
+      dispatch(requestArticles);
+    }, [])
+
+    
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
   return (
     <div className="news-container">
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+import store from './reducers/store.js';
+import { Provider } from 'react-redux';
+
+
+
+//the value of the store property should be the imported store 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+
+      <App />
+
+    </Provider>
   </React.StrictMode>,
 );
